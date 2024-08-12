59c53fd75a3b7d80c030e1a24917980ee0955785
SamZeb <gtalevel2020@gmail.com>
2024-08-12 16:16:06 -0400

    mensaje

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-let variable = 1;
-let nombre = "samuel";
+let suma = 2 + 2
+let resta = 8 - 2
+let multiplicar = 8 * 7
+let division = 6 / 4
+
+let entero = 4 + 4
+let decimal = 5 / 2
+let negativo = 2 - 9
+
+let miNombre = "Samuel"
+let mayorQue = 3 > 1
+let menorQue = 8 < 2
+let nulo = null
+let mink 
+
+let negacion = !6 > 9
+let afirmacion = 7 < 9 && 4 > 1
+let doble = 9 < 5 || 7 < 9 
