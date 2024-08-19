ec4bff32dc519f9429d32b26a2ce772575e6cc6b
SamZeb <gtalevel2020@gmail.com>
2024-08-19 16:18:51 -0400

    claseDeFunciones

---
+++ funciones.js
@@ -0,0 +1,61 @@
+/* function suma(numeroUno, numeroDos) {
+    return numeroUno + numeroDos
+} */
+
+let suma = (n1, n2) => n1 + n2
+
+/* console.log(suma(5, 5)) */
+
+/* function resta(n1, n2) {
+    return n1 - n2
+}
+
+console.log(resta(8, 2)) */
+
+let resta = (n1, n2) => n1 - n2
+
+/* function multiplicar(n1, n2) {
+    return n1 * n2
+}
+
+console.log(multiplicar(7, 5)) */
+
+let multiplicar = (n1, n2) => n1 * n2
+
+/* function division(n1, n2) {
+    return n1 / n2
+}
+
+console.log(division(4, 4)) */
+
+let division = (n1, n2) => n1 / n2
+
+/* function doble(n1) {
+    return n1 * 2
+}
+
+console.log(doble(4)) */
+
+let doble = (n1) => n1 * 2
+
+/* function mayor(n1, n2) {
+    if (n1 > n2) {
+        return n1
+    } else {
+        return n2
+    }
+}
+console.log(mayor(5, 9)) */
+
+let mayor = (n1, n2) => n1 > n2 ? n1 : n2
+
+function puedePasar(edad, acomp) {
+    if (edad >= 18) {
+        return true
+    } else if (edad >= 15 && acomp){
+        return true
+    } else {
+         return false
+    }
+}
+console.log(puedePasar(13, true))
