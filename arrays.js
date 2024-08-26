89a9756b69dacec030b0314108da54dc366c4a2e
SamZeb <gtalevel2020@gmail.com>
2024-08-26 16:09:10 -0400

    arreglos

---
+++ arrays.js
@@ -0,0 +1,27 @@
+/* let array = [true, 6, 'Hola']
+console.log(array[0])
+
+let miNombre = ['Samuel', 'Gregorio', 'Zambrano', 'Soto']
+console.log(miNombre)
+
+let misDatos = ['Samuel', 16, 169, 'Pizza']
+misDatos.push('Soto')
+console.log(misDatos) */
+
+/* let array = [true, 6, 'Hola']
+let ultimoValor = array.pop()
+console.log(ultimoValor)
+console.log(array) */
+
+/* let array = [true, 6, 'Hola']
+array.unshift(78)
+console.log(array) */
+
+/* let array = [true, 6, 'Hola']
+let primerValor = array.shift()
+console.log(primerValor)
+console.log(array) */
+
+let array = [true, 6, 'Hola', 6]
+
+console.log(array.lastIndexOf(6))
---
+++ ciclos.js
@@ -0,0 +1,27 @@
+/* for (i = 0; i <= 5; i++){
+    console.log('Hola')
+} */
+
+/* for (i = 1; i < 6; i++){
+    console.log(i)
+} */
+
+/* for (i = 1; i < 11; i++){
+    console.log(i * 8)
+} */
+
+/* for (i = 1; i < 11; i++) {
+    console.log('8x' + i + '= ' + i * 8)
+} */
+
+/* let acum = 0
+for (i = 1; i <= 10; i++){
+    acum = acum + i
+    console.log(acum)
+} */
+
+let i = 0
+while (i < 5) {
+    i++
+    console.log(i)
+}
